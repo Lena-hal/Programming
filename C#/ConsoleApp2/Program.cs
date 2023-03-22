7146145493c84a7f157a13aeaf015696eb2fab93
Lena-hal <78831891+Lena-hal@users.noreply.github.com>
2023-03-22 19:28:57 +0100

    another code

---
+++ C#/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            string b = Console.ReadLine();
+            bool z  = int.TryParse(b, out a);
+            Console.WriteLine(z);
+        }
+    }
+}
