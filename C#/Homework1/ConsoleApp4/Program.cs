7701c4d7b3d19134b49bfb65f492fd36ca1a1b70
Lena-hal <78831891+Lena-hal@users.noreply.github.com>
2023-03-28 20:40:47 +0200

    homework

---
+++ C#/Homework1/ConsoleApp4/Program.cs
@@ -0,0 +1,52 @@
+﻿//uživatel zadava čisla mezi jedničkou a stem počet vam řekne dopředu udělejte ciferný součet
+//udělat pomoci tříd a nebo metod
+using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocethodnot=0, soucet=0;
+            Console.WriteLine("Kolik Chcheš zadat hodnot?");
+            while (!int.TryParse(Console.ReadLine(), out pocethodnot))
+            {
+                Console.WriteLine("Neplatna hodnota");
+            }
+            for (int i= 0; i < pocethodnot; i++)
+            {
+                int cislo = 0;
+                Console.WriteLine("zadej cislo: ");
+                while (!int.TryParse(Console.ReadLine(), out cislo)) {
+                    Console.WriteLine("Neplatna hodnota");
+                }
+
+                Number classnum = new Number(cislo);
+                soucet += classnum.cifSoucet();
+            }
+            Console.WriteLine("Ciferný součet je: " + soucet);
+            
+
+            
+        }
+    }
+    public class Number
+    {
+        private int cislo;
+        public Number(int num)
+        {
+            cislo = num;
+        }
+        public int cifSoucet()
+        {
+            int soucet = 0;
+            while (cislo > 0)
+            {
+                soucet += cislo % 10;
+                cislo = cislo / 10;
+            }
+            return soucet;
+        }
+    }
+}
